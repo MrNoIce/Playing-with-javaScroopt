0eb00d6817ad99e6399a827c5c175fc90c4538f5
Jake Scott <bemybanker@gmail.com>
2019-06-01 13:00:24 -0500

    First round of playing with javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+let car = {
+    type: "ford",
+    make: "mustang",
+    color: "red",
+    year: 2015
+}
+console.log(car)
+
+const animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
+
+console.log(animalNames)
+
+const sampson = {
+      food: "tums",
+      car: "tricycle"
+}
+
+const timmy = {
+    food: "salmon",
+    car: "bently"
+}
+
+const jimmy = {
+    food: "candy",
+    car: "turtle"
+}
+
+const limmy = {
+	food: "slasberry",
+    car: "bicycle"
+}
+
+const time = ["samspson", "timmy", "jimmy", "limmy"]
+
+console.log(time)
